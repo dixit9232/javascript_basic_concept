865fac620f01fbe82d986aaf8a8c7c0d0a9a5ada
Dixit Pambhar <dixitpambhar9232@gmail.com>
2024-12-12 19:25:49 +0530

    learning data types

---
+++ 1_datatypes.js
@@ -0,0 +1,26 @@
+
+// for number
+var number = 10;
+console.log(number);
+
+// for String
+var courseName = "JavaScript";
+console.log(courseName);
+
+// for bool
+var isLoading = false;
+console.log(isLoading);
+
+// undefine
+var js;
+console.log(js);
+
+// for null 
+var badMemories = null;
+console.log(badMemories);
+
+// type of
+
+var courseName = "JavaScript";
+console.log(courseName);
+console.log(typeof courseName);
