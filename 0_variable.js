d97d8a6a4f7c956d6efcf0363bb9116aa93ae678
Dixit Pambhar <dixitpambhar9232@gmail.com>
2024-12-12 18:57:23 +0530

    start learning javascript

---
+++ 0_variable.js
@@ -0,0 +1,25 @@
+
+//valid
+var myAge = 24;
+console.log(myAge);
+
+//valid
+var my_name = "dixit";
+console.log(my_name);
+
+
+//valid
+var _myName = "Dixit";
+console.log(_myName);
+
+//valid
+var $cityName = "Surat";
+console.log($cityName);
+
+// invalid
+/**
+ * 
+ * We are never use a number and special symbol (exclude $) to define variable
+ * 
+ * 
+ *  */ 
