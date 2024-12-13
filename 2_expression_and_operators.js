f58852372ad5c8a86ebdbaf2cb5c1855999135b1
Dixit Pambhar <dixitpambhar9232@gmail.com>
2024-12-13 18:58:53 +0530

    learn expression and operator

---
+++ 2_expression_and_operators.js
@@ -0,0 +1,42 @@
+// assignment operator
+var x = 5;
+/**
+ *
+ * we can assign a value 5 to x using the assignment operator (=)
+ *
+ *  */
+
+// arithmetic operator
+
+// addition (+) operator
+var a = 5;
+var b = 3;
+var sum = a + b;
+console.log(sum);
+
+// subtraction (-) operator
+var a = 5;
+var b = 3;
+var sub = a - b;
+console.log(sub);
+
+// multiplication (*) operator
+var a = 5;
+var b = 3;
+var multiPlication = a * b;
+console.log(multiPlication);
+
+// division (/) operator
+var a = 5;
+var b = 3;
+var division = a / b;
+console.log(division);
+
+// modulus (%) operator
+var a = 5;
+var b = 3;
+var modulus = a % b;
+console.log(modulus);
+
+
+
