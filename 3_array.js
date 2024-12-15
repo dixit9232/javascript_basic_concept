7b9323606b5c8d15bd5c2bc768d645a293aeb08a
Dixit Pambhar <dixitpambhar9232@gmail.com>
2024-12-15 12:36:13 +0530

    learn array and for in,of and each method and also map method

---
+++ 3_array.js
@@ -0,0 +1,36 @@
+const fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+
+/*
+/// for of loop
+console.log("For of Loop");
+for (const items of fruits) {
+  console.log(items);
+}
+
+/// for in loop
+console.log("\n\nFor In Loop");
+
+for (const items in fruits) {
+  console.log(items);
+}
+
+
+
+
+/// for each loop
+console.log("\n\nFor Each Loop");
+fruits.forEach((element, index) => {
+  console.log(`${element} ${index}`);
+});
+
+
+
+/// map method
+console.log("\n\nmap method");
+const newArray = fruits.map((element, index) => {
+   return (`${element} ${index}`);
+});
+
+console.log(newArray);
+
+*/
